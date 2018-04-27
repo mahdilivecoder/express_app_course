5161918bacd819dd5458cda0f03d970bded07d73
livecoder <m.dev76@yahoo.com>
2018-04-27 16:32:40 +0430

    update

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+
+//hbs seting
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+//maintenance
+app.use('',(req,resp,next)=>{
+    resp.render('maintenance.hbs');
+});
+//seting and using static files
+app.use(express.static(__dirname + '/static'));
+app.use('',(req,resp,next)=>{
+    var now=new Date().toString();
+    //put date time to var log
+    var log='Date is:'+ now + req.url;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(err)=>{
+        if(err){
+            console.log('Cannot append file');
+        }
+    });
+    next();
+});
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('uppercase',(text)=>{
+    return text.toUpperCase();
+});
+app.get('/',(req,resp,next)=>{
+   resp.render('home.hbs',{
+       pageTitle:'This is a home page',
+       WelcomeMessage:'Welcome to my page',
+       title:req.url
+   });
+});
+
+app.get('/web',(req,resp,next)=>{
+   resp.render('web.hbs',{
+       pageTitle:'This is a one web site',
+       title:req.url
+   }) ;
+});
+
+app.get('/about',(req,resp,next)=>{
+   resp.send('<h1>About my website</h1>');
+});
+app.listen(3000,()=>{
+   console.log('App runing on port 3000');
+
+});
